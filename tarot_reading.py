3f58132f2abaedd650fd75e5bf1508fee37241b1
And89 <jff_team@hotmail.it>
2022-04-12 15:59:45 +0200

    Create tarot_reading.py

---
+++ tarot_reading.py
@@ -0,0 +1,22 @@
+'''
+0. Intro
+1. creare le carte
+2. una descrizione per ogni carta richiamabile con un comando
+3. far pescare le carte
+4. mandarle in stampa con questo formato:
+ ----------------
+|                |
+|                |
+|                |
+|                |      
+|      carta     |
+|                |
+|                |
+|                |
+|                |
+|                |
+ ----------------
+
+ 5. scrivere la lettura per ogni carta
+
+'''
