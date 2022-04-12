f7522186d4b86b0538738cc69d9cbe8aa1de03ff
And89 <jff_team@hotmail.it>
2022-04-12 17:01:49 +0200

    Update tarot_reading.py

--- tarot_reading.py
+++ tarot_reading.py
@@ -1,8 +1,12 @@
 '''
-0. Intro
+0. Intro --> Le carte danno un aiuto su come poter affrontare
+    un problema, non fanno divinazione. La domanda domande deve essere: 
+    come posso fare per uscire da una determ. situazione?
 1. creare le carte
 2. una descrizione per ogni carta richiamabile con un comando
-3. far pescare le carte
+3. far pescare le carte da 1 o 3
+3.1 se una carta, scrivere il significato in sintesi
+3.2 se tre carte, pass, pres, fut
 4. mandarle in stampa con questo formato:
  ----------------
 |                |
@@ -21,26 +25,56 @@
 
 '''
 cards = {
-    '': 'Le Mat', 
-    1: 'Le Bateleur', 
-    2: 'La Papesse', 
-    3: 'L\'Imperatrice', 
-    4: 'L\'Empereur', 
-    5: 'Le Pape', 
-    6: 'L\'Amoureux',
-    7: 'Le Chariot',
-    8: 'La Justice',
-    9: 'L\'Heremite',
-    10: 'La Rode de Fortune',
-    11: 'La Force',
-    12: 'Le Pendu',
-    13: '',
-    14: 'Temperance',
-    15: 'Le Diable',
-    16: 'La Maison Diev',
-    17: 'Le Toille',
-    18: 'La Lune',
-    19: 'Le Soleil',
-    20: 'Le Iugement',
-    21: 'Le Monde'
+    0: ' " " Le Mat', 
+    1: 'I Le Bateleur', 
+    2: 'II La Papesse', 
+    3: 'III L\'Imperatrice', 
+    4: 'IIII L\'Empereur', 
+    5: 'V Le Pape', 
+    6: 'VI L\'Amoureux',
+    7: 'VII Le Chariot',
+    8: 'VIII La Justice',
+    9: 'VIIII L\'Heremite',
+    10: 'X La Rode de Fortune',
+    11: 'XI La Force',
+    12: 'XII Le Pendu',
+    13: 'XIII ',
+    14: 'XIIII Temperance',
+    15: 'XV Le Diable',
+    16: 'XVI La Maison Diev',
+    17: 'XVII Le Toille',
+    18: 'XVIII La Lune',
+    19: 'XVIIII Le Soleil',
+    20: 'XX Le Iugement',
+    21: 'XI Le Monde'
     }
+
+definitions = {
+0: 'Va con ciò che posside in giro per il Mondo confiducia.',
+1: 'Ha tutto in mano, ha tutte le possibilità. Sta a lui a sceglire la cosa giusta per raggiungere il suo scopo',
+2: 'Sta studiando, è ancora ferma. E\' un momento di stasi.',
+3: 'Energia femminile allo stato puro',
+4: 'Potere in terra. Potere materiale.',
+5: 'Collegamento tra il terreno e il divino. Carta di passaggio ad un piano superiore.',
+6: 'La scelta. Ad esempio tra vecchio e nuovo.',
+7: 'la dimostrazione del proprio successo.',
+8: 'Tutto ciò che è inerente alla giustizia.',
+9: 'Ti invita a far luce sul passato',
+10: 'Momento di stallo che ha bisogno di un qualcosa per ripartire(la ruota)',
+11: 'Indica la forza morale, interiore.',
+12: 'Ci sono problemi da risolvere con il tuo albero genealogico',
+13: 'La trasformazione',
+14: 'La guarigione',
+15: 'Gli eccessi (ribellione, anticonformismo, riuscire a vedere oltre etc.)',
+16: 'Rappresenta il tuo corpo. Può essere una grande festa oppure l\'uscita da qualcosa di brutto',
+17: 'Il tuo posto.',
+18: 'La madre; rappresenta tutte le cose nascoste, interiori, profonde',
+19: 'Il padre; rappresenta tutto ciò che c\'è fuori',
+20: 'Il giudizio o la chiamata',
+21: 'Il completamento del tutto'
+
+
+
+
+
+}
