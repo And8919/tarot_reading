8e9927d29bfe586af210a3405cccc3b3c55828b4
And89 <jff_team@hotmail.it>
2022-04-12 16:18:10 +0200

    Update tarot_reading.py

--- tarot_reading.py
+++ tarot_reading.py
@@ -20,3 +20,25 @@
  5. scrivere la lettura per ogni carta
 
 '''
+cards = {
+    '': 'Le Mat', 
+    1: 'Le Bateleur', 
+    2: 'La Papesse', 
+    3: 'L\'Imperatrice', 
+    4: 'L\'Empereur', 
+    5: 'Le Pape', 
+    6: 'L\'Amoureux',
+    7: 'Le Chariot',
+    8: 'La Justice',
+    9: 'L\'Heremite',
+    10: 'La Rode de Fortune',
+    11: 'La Force',
+    12: 'Le Pendu',
+    13: '',
+    
+    
+    
+    
+    
+    
+    }
